f272f33d91833abf05e3a7f2406952b183e1fce7
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-01-07 20:11:23 -0500

    installed compression

--- node-server/index.js
+++ node-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const database = require('./database');
+const compression = require('compression');
 
 
 // Constants
@@ -8,6 +9,7 @@ const port = 8080;
 // App
 const app = express();
 app.use(express.json());
+app.use(compression());
 
 // Routes import
 const inventoryRouter = require('./api/inventory');
