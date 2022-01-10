190af7fab7eba5666197386c43e2e5cb829fae34
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-01-10 18:38:02 -0500

    trying to add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,13 @@
 import './App.css';
-import React, {useState} from "react";
-import TextField from '@mui/material/TextField';
-import { InputLabel, MenuItem, Select, Button } from '@mui/material';
-
+import React from "react";
+import InventoryAdd from './Components/InventoryAdd';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
-  const [groups, setGroups] = useState([]);
-  const [groupPicked, setGroupPicked] = useState(0);
-  const handleChange = (event) => {
-    setGroupPicked(event.target.value);
-  };
   return (
-    <div className="App">
-      <h1>Inventory</h1>
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <br/>
-      <br/>
-      <TextField id="outlined-basic" label="Quantity" variant="outlined" />
-      <br/>
-      <br/>
-      <InputLabel id="demo-simple-select-label">Group</InputLabel>
-      <Select
-        style={{minWidth: 200}}
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={groupPicked}
-        label="Group"
-        onChange={handleChange}
-      >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
-      </Select>
-      <br/>
-      <br/>
-
-      <Button variant="outlined">Submit</Button>
-
-    </div>
+    <Router>
+      <Route path="/users" component={InventoryAdd} />
+    </Router>
   );
 }
 
---
+++ frontend/src/Components/InventoryAdd.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import TextField from '@mui/material/TextField';
+import { InputLabel, MenuItem, Select, Button } from '@mui/material';
+
+function InventoryAdd() {
+  const [groups, setGroups] = useState([]);
+  const [groupPicked, setGroupPicked] = useState(0);
+  const handleChange = (event) => {
+    setGroupPicked(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Inventory</h1>
+      <TextField id="outlined-basic" label="Name" variant="outlined" />
+      <br/>
+      <br/>
+      <TextField id="outlined-basic" label="Quantity" variant="outlined" />
+      <br/>
+      <br/>
+      <InputLabel id="demo-simple-select-label">Group</InputLabel>
+      <Select
+        style={{minWidth: 200}}
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={groupPicked}
+        label="Group"
+        onChange={handleChange}
+      >
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+      <br/>
+      <br/>
+
+      <Button variant="outlined">Submit</Button>
+
+    </div>
+  );
+}
+
+export default InventoryAdd;
